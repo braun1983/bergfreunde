d8481885008a88ac357b1c04c6e35922ab329f64
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2026-05-05 20:08:24 -0300

    Footer, Sobre, Correção de bugs
    Correção de bugs no layout.
    E ajustes/Estilização no SObre.

--- src/components/sections/Sobre/Sobre.jsx
+++ src/components/sections/Sobre/Sobre.jsx
@@ -5,24 +5,24 @@ function Sobre() {
         <section className='container_about'>
             <div className='container_text'>
                 <h3 className='title'>Sobre</h3>
-                
+
                 <p className='about_text'>Fundado nas montanhas de Domingos Martins,
                     o Bergfreunde é mais que um grupo de dança;
                     é o guardião de um legado que atravessou oceanos.
                     Através da música tradicional, trajes meticulosamente
                     recriados e a alegria contagiante da cultura alemã,
                     mantemos viva a chama dos nossos antepassados.</p>
-                <p>Cada coreografia conta uma história de colheita,
+                <p className='about_text'>Cada coreografia conta uma história de colheita,
                     amor e comunidade, trazendo o espírito dos vilarejos europeus
                     para os palcos brasileiros com autenticidade e maestria.</p>
 
-                    <div>
-                        <p> <span> 40+</span> Anos de história</p>
-                        
-                    </div>
+                <div>
+                    <p className='destaque'> <span> 40+</span> Anos de história</p>
+
+                </div>
             </div>
             <div>
-<img className='img_casal' src={casal} alt="Casal dançando!"/>
+                <img className='img_casal' src={casal} alt="Casal dançando!" />
 
             </div>
         </section>
