1e363a799cb5099e1c6b23cc53671805180e2345
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2026-05-06 12:09:46 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/braun1983/bergfreunde',
+  base: '/bergfreunde/',
   plugins: [react()],
 })
