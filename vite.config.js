6b48a75efe1b05664ade753bdc08a928cb8fef8a
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2026-05-06 12:01:06 -0300

    correções para deplou

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nome-do-repo/',
+  base: '/braun1983/bergfreunde',
   plugins: [react()],
 })
