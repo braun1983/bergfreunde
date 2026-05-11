03b7982d36f420b690444c6ed8730e87ecb50244
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2026-05-11 20:43:12 -0300

    Atualização Patrocinadores

--- src/components/sections/Patrocinadores/Patrocinadores.jsx
+++ src/components/sections/Patrocinadores/Patrocinadores.jsx
@@ -1,12 +1,18 @@
 import './Patrocinadores.css'
-import hemo from '../../../assets/img/patrocinadores/hemo.jpeg'
+import hemo from '../../../assets/img/patrocinadores/hemo.png'
 import rotary from '../../../assets/img/patrocinadores/rotary.png'
-import blackTucano from '../../../assets/img/patrocinadores/black.jpg'
+import blackTucano from '../../../assets/img/patrocinadores/blacktucanocoffe.png'
+import tata from '../../../assets/img/patrocinadores/tata.png'
+import tiaTerezinhaSitio from '../../../assets/img/patrocinadores/tiaterezinhasitio.jpeg'
+import tiaTerezinhaPapelaria from '../../../assets/img/patrocinadores/tiaterezinhapapelaria.jpeg'
 
 const patrocinadores = [
     { nome: 'Hemo Laboratorio', logo: hemo },
     { nome: 'Rotary International', logo: rotary },
     { nome: 'Black Tucano Coffee Roasters', logo: blackTucano },
+    { nome: 'Sítio Tia Teresinha', logo: tiaTerezinhaSitio },
+    { nome: 'Laticínios Tatá', logo: tata },
+    { nome: 'Papelaria Tia Teresinha', logo: tiaTerezinhaPapelaria },
 ];
 
 function Patrocinadores() {
