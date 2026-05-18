66bd26078b239560e919372930467c5343fcc683
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2026-05-18 18:11:45 -0300

    Atualização sobre, história/marcos, integrantes

--- src/components/sections/HomePage/Sobre/Sobre.jsx
+++ src/components/sections/HomePage/Sobre/Sobre.jsx
@@ -1,34 +1,40 @@
-import casal from '../../../../assets/img/imgSobre.png'
-import './Sobre.css'
+import casal from "../../../../assets/img/imgSobre.png";
+import "./Sobre.css";
 function Sobre() {
+  return (
+    <section className="container_about" id="sobre">
+      <div className="container_text">
+        <h3 className="title">Sobre</h3>
 
-    
-    return (
-        <section className='container_about' id="sobre">
-            <div className='container_text'>
-                <h3 className='title'>Sobre</h3>
+        <p className="about_text">
+          Primeiro grupo de danças folclóricas germânicas criado no estado do
+          Espírito Santo, o Bergfreunde estreou em 25 de fevereiro de 1984 com o
+          objetivo de resgatar, preservar e divulgar a cultura trazida pelos
+          imigrantes germânicos à região de Domingos Martins, que estava sendo
+          esquecidas como consequência da II Guerra Mundial e da campanha de
+          nacionalização de Vargas.
+        </p>
+        <p className="about_text">
+          Além da divulgação da cultura em nosso estado, pelo país e pelo mundo,
+          o Grupo Folclórico Bergfreunde atuou e atua junto à comunidade
+          oferecendo oportunidades para que os cidadãos possam desenvolver
+          atividades culturais , que lhes propiciem novas experiências
+          culturais, o relacionamentos com pessoas de diferentes esferas
+          culturais, intelectuais e sociais.
+        </p>
 
-                <p className='about_text'>Fundado nas montanhas de Domingos Martins,
-                    o Bergfreunde é mais que um grupo de dança;
-                    é o guardião de um legado que atravessou oceanos.
-                    Através da música tradicional, trajes meticulosamente
-                    recriados e a alegria contagiante da cultura alemã,
-                    mantemos viva a chama dos nossos antepassados.</p>
-                <p className='about_text'>Cada coreografia conta uma história de colheita,
-                    amor e comunidade, trazendo o espírito dos vilarejos europeus
-                    para os palcos brasileiros com autenticidade e maestria.</p>
-
-                <div>
-                    <p className='destaque'> <span> 40+</span> Anos de história</p>
-
-                </div>
-            </div>
-            <div>
-                <img className='img_casal' src={casal} alt="Casal dançando!" />
-
-            </div>
-        </section>
-    )
+        <div>
+          <p className="destaque">
+            {" "}
+            <span> 40+</span> Anos de história
+          </p>
+        </div>
+      </div>
+      <div>
+        <img className="img_casal" src={casal} alt="Casal dançando!" />
+      </div>
+    </section>
+  );
 }
 
-export default Sobre
+export default Sobre;
